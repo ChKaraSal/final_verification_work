65bee9bc31865d3b88a19e34426a09750f12fa0c
chayan <chayan.karasal@internet.ru>
2023-02-13 15:13:57 +0700

    programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Final Work: ");
+string[] array1 = new string[4] {"1234", "1567", "-2", "computer sciense"};
+string[] array2 = new string[array1.Length];
+
+void GetArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+GetArray(array1, array2);
+PrintArray(array2);
